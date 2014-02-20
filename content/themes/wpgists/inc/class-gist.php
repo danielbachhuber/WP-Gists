184a5df374fb7e2f45c03ce3a985a6a20cda9dda
Daniel Bachhuber <d@danielbachhuber.com>
2014-02-20 15:46:37 -0800

    More helpful methods for our `Gist`

--- content/themes/wpgists/inc/class-gist.php
+++ content/themes/wpgists/inc/class-gist.php
@@ -5,6 +5,8 @@
 
 class Gist {
 
+	public static $post_type = 'gist';
+
 	private $_post;
 
 	public function __construct( WP_Post $post ) {
@@ -14,12 +16,78 @@ class Gist {
 	}
 
 	/**
+	 * Get the ID for this gist
+	 * 
+	 * @return int
+	 */
+	public function get_id() {
+		return $this->_post->ID;
+	}
+
+	/**
+	 * Get the content of this Gist
+	 * 
+	 * @return string
+	 */
+	public function get_content() {
+		return $this->_post->post_content;
+	}
+
+	/**
 	 * Get the description for this Gist
 	 *
 	 * @return string
 	 */
 	public function get_description() {
 		return $this->_post->post_excerpt;
+	}
+
+	/**
+	 * Get the permalink for this Gist
+	 * 
+	 * @return string
+	 */
+	public function get_permalink() {
+		return apply_filters( 'the_permalink', get_permalink( $this->get_id() ) );
+	}
+
+	/**
+	 * Create a Gist
+	 * 
+	 * @return object|WP_Error
+	 */
+	public function create( $args ) {
+
+		$defaults = array(
+			'content'          => '',
+			'description'      => '',
+			);
+		$args = array_merge( $defaults, $args );
+
+		$post = array(
+			'post_content'     => $args['content'],
+			'post_excerpt'     => $args['excerpt'],
+			'post_type'        => self::$post_type,
+			);
+		$post_id = wp_insert_post( $post );
+		if ( is_wp_error( $post_id ) ) {
+			return $post_id;
+		}
+
+		$post = get_post( $post_id );
+		return new Gist( $post );
+	}
+
+	/**
+	 * Export all of the fields for the API
+	 */
+	public function export_for_api() {
+		return array(
+			'id'               => $this->get_id(),
+			'content'          => $this->get_content(),
+			'description'      => $this->get_description(),
+			'permalink'        => $this->get_permalink(),
+			);
 	}
 
 }
