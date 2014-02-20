7944b4dca8aef7b1364449efdfbea383befe3892
Daniel Bachhuber <d@danielbachhuber.com>
2014-02-20 15:24:28 -0800

    Use the proper filter

--- content/mu-plugins/environment.php
+++ content/mu-plugins/environment.php
@@ -11,6 +11,6 @@ add_filter( 'pre_option_stylesheet', $wpgists_dont_change_theme );
 add_filter( 'pre_option_template', $wpgists_dont_change_theme );
 
 
-add_filter( 'permalink_structure', function() {
+add_filter( 'pre_option_permalink_structure', function() {
 	return '/%year%/%monthnum%/%day%/%postname%/';
 });
