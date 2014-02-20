2c572f862e8ff9281cdd4788f4204f244683c9b1
Daniel Bachhuber <d@danielbachhuber.com>
2014-02-20 15:23:56 -0800

    Set a default permalink structure

--- content/mu-plugins/environment.php
+++ content/mu-plugins/environment.php
@@ -9,3 +9,8 @@ $wpgists_dont_change_theme = function() {
 };
 add_filter( 'pre_option_stylesheet', $wpgists_dont_change_theme );
 add_filter( 'pre_option_template', $wpgists_dont_change_theme );
+
+
+add_filter( 'permalink_structure', function() {
+	return '/%year%/%monthnum%/%day%/%postname%/';
+});
