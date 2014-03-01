c366d3e2644dfc33814cb4be87b98dac0aec915e
Zebulon Young <zebulonj@gmail.com>
2014-03-01 09:48:51 -0800

    Add data bindings and save to gist edit form.

--- content/themes/wpgists/js/app/gists/gist_edit.view.js
+++ content/themes/wpgists/js/app/gists/gist_edit.view.js
@@ -1,11 +1,36 @@
-define( ['backbone', 'handlebars', 'text!app/gists/gist_edit.mustache'], function( Backbone, Handlebars, template ) {
+define( ['backbone', 'jquery', 'handlebars', 'text!app/gists/gist_edit.mustache'], function( Backbone, $, Handlebars, template ) {
 	'use strict';
 
 	template = Handlebars.compile( template );
 
 	var GistEditView = Backbone.View.extend({
+		events: {
+			'click [data-control="save"]': "save",
+
+			'blur input[data-bind]': "update",
+			'blur textarea[data-bind]': "update"
+		},
+
 		initialize: function( options ) {
 			this.$el.html( template( this.model.toJSON() ) );
+		},
+
+		save: function( e ) {
+			console.log( "Saving..." );
+
+			this.model.save()
+				.done( function() {
+					console.log( "Saved!" );	// TODO: Add proper user feedback.
+				});
+		},
+
+		update: function( e ) {
+			var model = this.model, $target = $( event.target ), attr = $target.attr( 'data-bind' ), value = $target.val(), updates = {};
+
+			// Passes `null` rather than empty string.
+			updates[ attr ] = ( value === '' ? null : value );
+
+			model.set( updates );
 		}
 	});
 
